9f7a4e27ed5e8e2d4f61c71ed5d3b35fedaa1973
Hải Sơn Nguyễn <nguyensonhai1009@gmail.com>
2020-10-01 11:01:09 +0700

    update

--- sqlquery/bookstorequery.sql
+++ sqlquery/bookstorequery.sql
@@ -1,9 +1,21 @@
 CREATE TABLE [Book] (
-    [Id]          BIGINT        IDENTITY (1, 1) NOT NULL,
+    [Id]          BIGINT   IDENTITY (1, 1) NOT NULL,
     [Title]       VARCHAR (50)  NULL,
     [Author]      VARCHAR (50)  NULL,
     [Genre]       VARCHAR (50)  NULL,
 	[Language]    VARCHAR (50)  NULL,
-	[Price]       INT NULL,
+	[Price]       INT			NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [Member] (
+    [Id]			BIGINT   IDENTITY (1, 1) NOT NULL,
+    [FirstName]     VARCHAR (50)  NULL,
+    [LastName]      VARCHAR (50)  NULL,
+    [DateOfBirth]   DATETIME	  NULL,
+	[Phone]			INT			  NULL,
+	[Email]			VARCHAR (50)  NULL,
+	[Address]       VARCHAR (50)  NULL,
+	[DateJoined]    DATETIME	  NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
